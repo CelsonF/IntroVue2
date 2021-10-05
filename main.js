2ebd32b81121e40c3025fb2dd3290f8abea11208
CelsonF <celson338540@gmail.com>
2021-10-05 19:49:25 -0300

    Add list render in page product

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        product:'Socks',
+        inStock:false,
+        ancorProd:false,
+        image:'./assets/vmSocks-green-onWhite.jpg',
+        colorsSocks:'Green socks',
+        onSale:true,
+        link:'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        details:["80% cotton","20% polyester","Gender-neutral"]
+        
+    }
+});
