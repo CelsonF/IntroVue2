4549520a1c88c55ddea0246395a515b4e8b87d3d
CelsonF <celson338540@gmail.com>
2021-10-06 21:00:36 -0300

    Add array of variables

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 var app = new Vue({
-    el:'#app',
-    data:{
-        product:'Socks',
-        inStock:false,
-        ancorProd:false,
-        image:'./assets/vmSocks-green-onWhite.jpg',
-        colorsSocks:'Green socks',
-        onSale:true,
-        link:'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        details:["80% cotton","20% polyester","Gender-neutral"]
-        
+    el: '#app',
+    data: {
+        product: 'Socks',
+        inStock: false,
+        ancorProd: false,
+        image: './assets/vmSocks-green-onWhite.jpg',
+        colorsSocks: 'Green socks',
+        onSale: true,
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+
     }
 });
